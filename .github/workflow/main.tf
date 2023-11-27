2482e64d64c58602d5ff9d4980ab42d5d3bc8a23
ZahaabAlim <Zahaabalim@gmail.com>
2023-11-27 11:25:23 +0530

    initial commit

---
+++ .github/workflow/main.tf
@@ -0,0 +1,24 @@
+#provider block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+#creating s3 bucket
+resource "aws_s3_bucket" "factorial_bucket" {
+  bucket = "zahaab-manual-bucket"
+  acl= "private"
+}
+#adding object to s3 bucket
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.factorial_bucket.bucket
+  key    = "factorial.py"
+  source = "factorial.py"
+}
